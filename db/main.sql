7e60b5ddf1ab756b9fea21e0d698e0b0fe3f1979
jungkihunm <khkh0130@gmail.com>
2022-07-15 13:26:42 +0900

    jkh sql 작성

--- db/main.sql
+++ db/main.sql
@@ -12,18 +12,32 @@ CONSTRAINT users_pkey PRIMARY KEY (user_no)
 );
 COMMENT ON COLUMN public.users.state IS '0 탈퇴 1 정상 2 계정잠금';
 
---메모
-CREATE TABLE public.note (
-	note_no serial4 NOT NULL,
-	note_data jsonb NULL DEFAULT '{"note_text": [{"content": null, "deadline": null,"note_checked":null}], "note_index": null}'::jsonb,
-	state int4 NOT NULL DEFAULT 1,
-	reg_dt timestamp NOT NULL DEFAULT now(),
-	user_no serial4 NOT NULL,
-    end_dt timestamp NOT NULL DEFAULT now(),
-	CONSTRAINT note_pkey PRIMARY KEY (note_no),
-	CONSTRAINT users_user_no_fkey FOREIGN KEY (user_no) REFERENCES public.users(user_no) ON DELETE CASCADE ON UPDATE CASCADE
+-- 기계정보
+create table machine(
+ma_no serial not null,
+ma_name varchar not null,
+user_no int4 not NULL,
+state integer not null DEFAULT 1,
+reg_dt timestamp not null DEFAULT now(),
+recent_dt timestamp not null DEFAULT now(),
+CONSTRAINT users_pkey PRIMARY KEY (ma_no)
+CONSTRAINT users_user_no_fkey FOREIGN KEY (user_no) REFERENCES public.users(user_no) ON DELETE CASCADE ON UPDATE CASCADE
 );
-COMMENT ON COLUMN public.note.state IS '0 삭제 1 정상';
-COMMENT ON COLUMN public.note.reg_dt IS '생성시간';
+COMMENT ON COLUMN public.machine.state IS '0 비활성 1 정상 2 전원꺼짐';
+COMMENT ON COLUMN public.machine.reg_dt IS '추가시간';
+COMMENT ON COLUMN public.machine.recent_dt IS '서버와 통신을 마지막으로 한시간';
+
+-- 수집정보
+CREATE TABLE Collection_info (
+	co_no serial4 NOT NULL,
+    co_misae NUMERIC,
+	co_humidity NUMERIC,
+	co_temperature NUMERIC,
+	state int4 NOT NULL DEFAULT 1,
+	reg_dt timestamp NOT NULL DEFAULT now(),	
+	CONSTRAINT Collection_info_pkey PRIMARY KEY (co_no),
+	CONSTRAINT machine_ma_no_fkey FOREIGN KEY (ma_no) REFERENCES public.machine(ma_no) ON DELETE CASCADE ON UPDATE CASCADE
+);
+COMMENT ON COLUMN public.note.state IS '0 삭제 1 정상 2 고장';
+COMMENT ON COLUMN public.note.reg_dt IS '추가시간';
 COMMENT ON COLUMN public.note.end_dt IS '최종수정시간';
-COMMENT ON COLUMN public.note.note_data IS '{"note_text": [{"content": null, "deadline": null,"note_checked":null}], "note_index": null}';
