4f2965dc764ce9b8b7d6aa7abff405abc7588131
jungkihun <khkh0130@gmail.com>
2022-07-25 17:35:51 +0900

    jkh node-schedule add

--- api/user/v1/openapi/getAirData.js
+++ api/user/v1/openapi/getAirData.js
@@ -2,7 +2,7 @@
 const jkh = require('../../../lib/jkh_function');
 const { Q, pool } = require('../../../../db/pg');
 
-
+//https://blog.naver.com/PostView.naver?blogId=qbxlvnf11&logNo=222489497378&categoryNo=0&parentCategoryNo=0&viewDate=&currentPage=1&postListTopCurrentPage=1&from=postView
 // todo (api load -> db save) 1h loof
 module.exports = {
 
