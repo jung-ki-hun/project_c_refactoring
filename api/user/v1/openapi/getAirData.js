369692764323b4e2c90e1aa351cf40bd43b75b61
jungkihun <khkh0130@gmail.com>
2022-07-25 17:29:08 +0900

    jkh openapi

---
+++ api/user/v1/openapi/getAirData.js
@@ -0,0 +1,9 @@
+// const app = express.Router();
+const jkh = require('../../../lib/jkh_function');
+const { Q, pool } = require('../../../../db/pg');
+
+
+// todo (api load -> db save) 1h loof
+module.exports = {
+
+}
