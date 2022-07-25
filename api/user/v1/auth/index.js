c4d9ef7a6e6d15321fd91a1b3077e4d854a00253
jungkihun <khkh0130@gmail.com>
2022-07-25 16:03:22 +0900

    jkh auth/login test제거

--- api/user/v1/auth/index.js
+++ api/user/v1/auth/index.js
@@ -6,20 +6,20 @@ var app = express.Router()
  * @description /login -> 로그인
  * @description /pwChage -> 비밀번호 변경
  */
-const test = async (req, res) => {
-    var ress = [];
-    for (let i = 0; i < 100; i++) {
-        ress.push({
-            context_id: i + 1,
-            data: "데이터값" + (i + 1) + "번",
-            title: "제목" + (i + 1) + "번째",
-            user: "사용자명",
-            count: 30,
-            dumy: req.body
-        });
-    }
-    return res.status(200).json(ress);
-}/// 테스트 함수
+// const test = async (req, res) => {
+//     var ress = [];
+//     for (let i = 0; i < 100; i++) {
+//         ress.push({
+//             context_id: i + 1,
+//             data: "데이터값" + (i + 1) + "번",
+//             title: "제목" + (i + 1) + "번째",
+//             user: "사용자명",
+//             count: 30,
+//             dumy: req.body
+//         });
+//     }
+//     return res.status(200).json(ress);
+// }/// 테스트 함수
 
 
 module.exports = (app) => {
